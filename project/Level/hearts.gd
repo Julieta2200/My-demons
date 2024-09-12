efb2065d50d3e83f48592139343982b90327a636
hovo-gharibjanyan <gharibjanyanhovhannes610@gmail.com>
2024-09-12 14:46:18 +0400

    SKill 2

--- project/Level/hearts.gd
+++ project/Level/hearts.gd
@@ -1,8 +1,11 @@
 extends Control
 
 var deactive_sprite = "res://assets/ui/Heart2.png"
+var active_sprite = "res://assets/ui/Heart1.png"
 
 @onready var hearts: Array[Dictionary] = [
+	{"texture": $"5", "active": false},
+	{"texture": $"4", "active": false},
 	{"texture": $"3", "active": true},
 	{"texture": $"2", "active": true},
 	{"texture": $"1", "active": true},
@@ -17,4 +20,14 @@ func remove():
 		if heart["texture"] == $"1":
 			$"../../AnimationPlayer".play("lost")
 		break
-	
+		
+func add_heart():
+	for i in range(hearts.size() - 1, -1, -1): 
+		var heart = hearts[i]
+		if heart["active"]:
+			continue 
+		heart["active"] = true
+		heart["texture"].visible = true
+		heart["texture"].texture = load(active_sprite)
+		break  
+
